f9391486e6987f30347f5d61a09be201c2d41175
romulofull <romulo.castro@gmail.com>
2024-07-30 19:23:22 -0500

    Recorriendo Vectores

--- js/VectoresyArrays.js
+++ js/VectoresyArrays.js
@@ -5,7 +5,6 @@ let miMatriz = [[1,2,3],
 [10,9,7]];
 console.log (miMatriz[1][1]);
 
-
 let vector = new Array(3);
 vector[0] = "Romulo, Asesor, Tennis, Uk";
 vector[1] = "Programador";
@@ -19,3 +18,15 @@ let persona1 = ["Lazo","Atletismo","Thriatlon"];
 let persona2 = ["Tete","Gym","Tennis"];
 let matriz = [persona0, persona1, persona2];
 console.log (matriz);
+
+let paisesLatam = ["Argentina", "Mexico", "Ecuador", "Brasil"];
+for (let i=0; i<paisesLatam.length; i++) {
+console.log (paisesLatam[i]);
+if (paisesLatam[i] == "Ecuador") {
+    console.log ("Estoy en el país donde naci");
+}
+}
+console.log(paisesLatam);
+
+
+
